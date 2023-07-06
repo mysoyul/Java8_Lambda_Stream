1c86d05f6a7bb39eca1e4a5a5fadab37c37f7402
mysoyul <vega2k_@naver.com>
2023-07-06 14:15:16 +0900

    FilteringApples update

--- src/main/java/lambdasinaction/_01lambda/basic2/FilteringApples.java
+++ src/main/java/lambdasinaction/_01lambda/basic2/FilteringApples.java
@@ -9,7 +9,8 @@ public class FilteringApples{
 
         List<Apple> inventory = Arrays.asList(new Apple(80,"green"),
                 new Apple(155, "green"),
-                new Apple(120, "red"));
+                new Apple(120, "red"),
+                new Apple(160, "red"));
 
         // 람다식 사용 [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
         filterApples(inventory, apple -> apple.getColor().equals("green"))
@@ -25,6 +26,18 @@ public class FilteringApples{
         // Method Reference 사용 [Apple{color='green', weight=155}]
         filterApples(inventory, FilteringApples::isHeavyApple);
 
+        Predicate<Apple> redApple = apple -> apple.getColor().equals("red");
+        Predicate<Apple> notRedApple = redApple.negate();
+
+        System.out.println("Predicate or() 메서드");
+        Predicate<Apple> orPredicate = notRedApple.or(a -> a.getWeight() > 150);
+        filterApples(inventory, orPredicate)
+                .forEach(System.out::println);
+
+        System.out.println("Predicate and() 메서드");
+        Predicate<Apple> andPredicate = notRedApple.and(a -> a.getWeight() > 150);
+        filterApples(inventory, andPredicate)
+                .forEach(System.out::println);
         // []
         List<Apple> weirdApples = filterApples(inventory, (Apple a) -> a.getWeight() < 80 ||
                 "brown".equals(a.getColor()));
