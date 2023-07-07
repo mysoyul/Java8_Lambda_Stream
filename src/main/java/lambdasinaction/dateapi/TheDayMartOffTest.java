c073e7a445f68f4c96c0c8526049a8c827166aba
mysoyul <vega2k_@naver.com>
2023-07-07 15:48:39 +0900

    TheDayMartOff add

--- src/main/java/lambdasinaction/dateapi/TheDayMartOffTest.java
+++ src/main/java/lambdasinaction/dateapi/TheDayMartOffTest.java
@@ -9,6 +9,7 @@ import static java.time.temporal.TemporalAdjusters.*;
 public class TheDayMartOffTest {
     public static void main(String[] args) {
         LocalDate today = LocalDate.now();
+        today = LocalDate.of(2023, 7, 24);
         System.out.println("현재날짜 = " + today);
 
         //with(TemporalAdjuster) TemporalAdjuster의 추상메서드 Temporal adjustInto(Temporal temporal)
@@ -18,16 +19,22 @@ public class TheDayMartOffTest {
 //                return null;
 //            }
 //        });
-        today.with(temporal -> {
-           //1. 기준을 되는 날짜를 구하기
+        LocalDate martDayOff = today.with(temporal -> {
+            //1. 기준을 되는 날짜를 구하기
             LocalDate theDay = LocalDate.from(temporal);
             //2. 두번째,네번째 일요일 날짜를 구하기
-            LocalDate secondDay = theDay.with(dayOfWeekInMonth(2, DayOfWeek.SATURDAY));
-            LocalDate fourthDay = theDay.with(dayOfWeekInMonth(4, DayOfWeek.SATURDAY));
+            LocalDate secondDay = theDay.with(dayOfWeekInMonth(2, DayOfWeek.SUNDAY));
+            LocalDate fourthDay = theDay.with(dayOfWeekInMonth(4, DayOfWeek.SUNDAY));
             //3. 기준날짜와 비교하기
-
+            if (theDay.isBefore(secondDay)) {
+                return secondDay;
+            } else if (theDay.isBefore(fourthDay)) {
+                return fourthDay;
+            } else {
+                return theDay.plusMonths(1).with(dayOfWeekInMonth(2, DayOfWeek.SUNDAY));
+            }
         });
 
-
+        System.out.println("다음 Mart 쉬는 날은 언제인가요? " + martDayOff);
     }
 }
