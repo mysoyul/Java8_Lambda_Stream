61966ab815a8a1ea201864efad891c39844f01a0
mysoyul <vega2k_@naver.com>
2023-07-07 14:10:13 +0900

    Grouping update

--- src/main/java/lambdasinaction/_02stream/collect/_04GroupingDishes.java
+++ src/main/java/lambdasinaction/_02stream/collect/_04GroupingDishes.java
@@ -67,7 +67,10 @@ public class _04GroupingDishes {
 
     //5.1 type별 그룹에서 가장 칼로리가 높은 Dish 찾기 - collectingAndThen() 사용
     private static Map<Dish.Type, Dish> mostCaloricDishesByTypeWithoutOptionals() {
-        return null;
+        return menu.stream()
+                .collect(groupingBy(getDishType(), collectingAndThen(
+                        maxBy(getDishComparator()), Optional::get
+                )));
     }
 
     //6. type별로 그룹핑하여 칼로리의 합계 내기
