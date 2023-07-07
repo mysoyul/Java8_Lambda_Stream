fd0e5cff1c37ab088c3a6c34eb661ed674db3fdd
mysoyul <vega2k_@naver.com>
2023-07-07 13:45:16 +0900

    Grouping update

--- src/main/java/lambdasinaction/_02stream/collect/_03Reducing.java
+++ src/main/java/lambdasinaction/_02stream/collect/_03Reducing.java
@@ -14,11 +14,13 @@ public class _03Reducing {
 
     //reducing 사용한 칼로리의 합
     private static int calculateTotalCalories() {
-        return menu.stream().collect(reducing(0, Dish::getCalories, (i, j) -> i + j));
+        return menu.stream()
+                .collect(reducing(0, Dish::getCalories, (i, j) -> i + j));
     }
     //reducing 인자로 method 참조를 사용한 칼로리의 합
     private static int calculateTotalCaloriesWithMethodReference() {
-        return menu.stream().collect(reducing(0, Dish::getCalories, Integer::sum));
+        return menu.stream()
+                .collect(reducing(0, Dish::getCalories, Integer::sum));
     }
     //map - reduce 사용
     private static int calculateTotalCaloriesWithoutCollectors() {
@@ -28,7 +30,6 @@ public class _03Reducing {
     }
     //mapToInt 사용
     private static int calculateTotalCaloriesUsingSum() {
-
         return menu.stream()
                 .mapToInt(Dish::getCalories) //IntStream
                 .sum();
--- src/main/java/lambdasinaction/_02stream/collect/_04GroupingDishes.java
+++ src/main/java/lambdasinaction/_02stream/collect/_04GroupingDishes.java
@@ -1,6 +1,7 @@
 package lambdasinaction._02stream.collect;
 
 import java.util.*;
+import java.util.function.Function;
 
 import static java.util.stream.Collectors.*;
 import static lambdasinaction._02stream.collect.Dish.menu;
@@ -22,12 +23,23 @@ public class _04GroupingDishes {
 
     //1. type별 그룹핑
     private static Map<Dish.Type, List<Dish>> groupDishesByType() {
-        return null;
+        return menu.stream()
+                .collect(groupingBy(Dish::getType));
     }
     //2. 칼로리별 그룹핑
     private static Map<CaloricLevel, List<Dish>> groupDishesByCaloricLevel() {
-        return null;
+        return menu.stream()
+                .collect(groupingBy(getCaloricLevelFunction()));
     }
+
+    private static Function<Dish, CaloricLevel> getCaloricLevelFunction() {
+        return dish -> {
+            if (dish.getCalories() <= 400) return CaloricLevel.DIET;
+            else if (dish.getCalories() <= 700) return CaloricLevel.NORMAL;
+            else return CaloricLevel.FAT;
+        };
+    }
+
     //3. type별로 그룹핑 후에 다시 칼로리별로 그룹핑
     private static Map<Dish.Type, Map<CaloricLevel, List<Dish>>> groupDishedByTypeAndCaloricLevel() {
         return null;
@@ -52,10 +64,6 @@ public class _04GroupingDishes {
 
     private static Map<Dish.Type, Set<CaloricLevel>> caloricLevelsByType() {
         return menu.stream().collect(
-                groupingBy(Dish::getType, mapping(
-                        dish -> { if (dish.getCalories() <= 400) return CaloricLevel.DIET;
-                        else if (dish.getCalories() <= 700) return CaloricLevel.NORMAL;
-                        else return CaloricLevel.FAT; },
-                        toSet() )));
+                groupingBy(Dish::getType, mapping(getCaloricLevelFunction(), toSet())));
     }
 }
