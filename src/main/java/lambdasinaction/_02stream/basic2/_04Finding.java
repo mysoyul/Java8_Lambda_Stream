602bb9005346fdd538182807746e6060d05fccea
mysoyul <vega2k_@naver.com>
2023-07-07 10:20:11 +0900

    Reducing update

--- src/main/java/lambdasinaction/_02stream/basic2/_04Finding.java
+++ src/main/java/lambdasinaction/_02stream/basic2/_04Finding.java
@@ -15,29 +15,32 @@ public class _04Finding {
         System.out.println(allMatchMenu());
         System.out.println(noneMatchMenu());
         
-        Optional<Dish> dish = findVegetarianDish();
-        dish.ifPresent(d -> System.out.println(d.getName()));
+        Optional<Dish> dishOptional = findVegetarianDish();
+        dishOptional.ifPresent(d -> System.out.println(d.getName()));
     }
 
     //1. anyMatch
     private static boolean isVegetarianFriendlyMenu(){
-        return menu.stream().anyMatch(Dish::isVegetarian);
+        return menu.stream()
+                .anyMatch(Dish::isVegetarian);
     }
     //2.allMatch
     private static boolean allMatchMenu(){
-
-        return false;
+        return menu.stream()
+                .allMatch(dish -> dish.getCalories() <= 800);
     }
 
     //3. noneMatch
     private static boolean noneMatchMenu(){
-
-        return false;
+        return menu.stream()
+                .noneMatch(dish -> dish.getCalories() > 800);
     }
     //4. findAny
     private static Optional<Dish> findVegetarianDish(){
-
-        return null;
+//        return Optional.empty();  //empty 한 Optional
+        return menu.stream()
+                .filter(Dish::isVegetarian)
+                .findAny();
     }
     
 }
--- src/main/java/lambdasinaction/_02stream/basic2/_05Reducing.java
+++ src/main/java/lambdasinaction/_02stream/basic2/_05Reducing.java
@@ -1,26 +1,72 @@
 package lambdasinaction._02stream.basic2;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-
+import lambdasinaction._02stream.basic1.Dish;
+import static lambdasinaction._02stream.basic1.Dish.menu;
 public class _05Reducing {
 
     public static void main(String... args) {
 
         //reduce - a + b 연산
         List<Integer> numbers = Arrays.asList(3, 4, 5, 1, 2);
+        //reduce() 메서드 구현
+        Integer sum = numbers.stream()
+                .reduce(0, (n1, n2) -> n1 + n2);
+        System.out.println("sum = " + sum);
+        //IntStream의 sum() 메서드 사용
+        sum = numbers.stream().mapToInt(Integer::intValue).sum();
+        System.out.println("sum2 = " + sum);
 
-        //reduce -  최소값
+        //sum = numbers.stream().flatMapToInt(number -> IntStream.of(number)).sum();
+        sum = numbers.stream().flatMapToInt(IntStream::of).sum();
+        System.out.println("sum3 = " + sum);
 
-        //reduce - 최대값
+        //reduce -  최대값
+        int max = numbers.stream().reduce(0, (n1,n2) -> Integer.max(n1,n2));
+        max = numbers.stream().reduce(0, Integer::max);
+        System.out.println("max = " + max);
 
+        //Stream의 min()
+        int minValue = numbers.stream().min(Integer::compareTo).get();
+        System.out.println("minValue = " + minValue);
 
-        Optional<Integer> min = numbers.stream().reduce(Integer::min);
-        min.ifPresent(System.out::println);
+        //reduce - 최소값
+        Optional<Integer> optional = numbers.stream().reduce(Integer::min);
+        minValue = optional.orElse(0);
+        System.out.println("minValue2 = " + minValue);
+        optional.ifPresent(System.out::println);
 
         //Dish 의  총 칼로리 합계를 구하는 여러가지 방법
+        //1. reduce() 함수로 구현
+        Integer totalValue = menu.stream() //Stream<Dish>
+                .map(Dish::getCalories) //Stream<Integer>
+                .reduce(0, (dish1, dish2) -> dish1 + dish2);
+        System.out.println("totalValue = " + totalValue);
+        //2. reduce() 함수에서 Integer.sum() 메서드 호출
+        totalValue = menu.stream()
+                .map(Dish::getCalories)
+                .reduce(Integer::sum) //Optional<Integer>
+                .get();
+        System.out.println("totalValue2 = " + totalValue);
+        //3. mapToInt()사용하여 IntStream 변환하여 sum() 호출
+        totalValue = menu.stream()
+                .mapToInt(Dish::getCalories) //IntStream
+                .sum();
+        System.out.println("totalValue3 = " + totalValue);
+        //4. Collectors의 summingInt() 호출
+        totalValue = menu.stream()
+                .collect(Collectors.summingInt(Dish::getCalories));
+        System.out.println("totalValue4 = " + totalValue);
+
+        IntSummaryStatistics statistics = menu.stream()
+                .collect(Collectors.summarizingInt(Dish::getCalories));
+        System.out.println("statistics = " + statistics);
 
 
 
